55c2a25ed7eed1b674b7a6df38d8c5a4be499305
Luca Antiga <luca.antiga@orobix.com>
2012-11-30 11:03:12 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hirop-compojure
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/hirop_compojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns hirop-compojure.core-test
+  (:use clojure.test
+        hirop-compojure.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
