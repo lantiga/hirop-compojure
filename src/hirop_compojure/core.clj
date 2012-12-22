53c3ff9414460f28fcc4081b8d5e03e3f839771b
Luca Antiga <luca.antiga@orobix.com>
2012-12-22 19:03:30 +0100

    Added REST api notes

--- src/hirop_compojure/core.clj
+++ src/hirop_compojure/core.clj
@@ -11,9 +11,9 @@
   (comp response vec))
 
 (defroutes hirop-routes
-  (GET "/doctypes" []
+  (GET "/get-doctypes" []
        (response (doctypes)))
-  (GET "/contexts" []
+  (GET "/get-contexts" []
        (response (contexts)))
   (POST "/clean-contexts" []
         (clean-contexts))
@@ -74,3 +74,85 @@
   (POST "/unselect" {{selection-id :selection-id doctype :doctype context-id :context-id} :params}
         (vec-response (unselect :selection-id selection-id :doctype doctype :context-id context-id))))
 
+
+;; TODO: rename from hirop-compojure.core to hirop-compojure.rpc
+;; TODO: implement hirop-compojure.rest
+
+;; The thing is that here we are using the session, but we probably shouldn't
+;; We should have a database made of atoms (a map of atoms, indexed by context-id (or session cookie in case of stateful)
+;; That database api should be specified as a protocol. Then we can make a memory-based (for embedded) and a Redis backend.
+
+;; This would require to rewrite session.clj (renaming it to store.clj), avoid using ring-session at all (apart from ids / connection info).
+
+;; Stateful should also be modified, too complex. It should probably rely on having a context-id uuid, like the REST.
+;; Which should be used to fetch the record in the store database.
+
+;; Expiral
+;; http://kotka.de/blog/2010/03/memoize_done_right.html
+;; https://github.com/clojure/core.cache
+
+;; REST API
+;; Remove create-context. Replace with external-ids and context name (mandatory) + meta (optional)
+;; at every call (we can always write a stateful js library later).
+;; A context that is not in memory (TODO: index by external ids and context name - in stateful the map is
+;; used as the context-id) has to be queried,
+;; otherwise we use the one in memory.
+;; We keep clear-context(s) as courtesy urls, just to clean memory.
+
+;; THE API:
+
+;; Login
+;; GET https://shirop.io/sessions/new
+;; POST https://shirop.io/sessions
+;; returns session-id
+
+;; Context id
+;; POST https://shirop.io/sessions/:session-id/contexts
+;; with app-id, context-name, external-ids and meta JSON payload; returns context-id
+
+;; DELETE https://shirop.io/sessions/:session-id/contexts
+
+;; clear contexts
+;; clear context
+;; commit
+;; push
+;; pull
+;; select
+;; unselect
+
+;; DELETE https://shirop.io/contexts/:context-id
+
+;; PUT https://shirop.io/contexts/:context-id/:doc-id
+;; POST https://shirop.io/contexts/:context-id/store
+;; POST https://shirop.io/store/contexts/:context-id
+;; PUT https://shirop.io/contexts/:context-id/selected/:selection-id/:doc-id
+;; DELETE https://shirop.io/contexts/:context-id/selected/:selection-id
+;; 
+;; GET https://shirop.io/contexts/:context-id/external
+;; GET https://shirop.io/contexts/:context-id/configurations
+;; GET https://shirop.io/contexts/:context-id/configurations/:doctype
+;; GET https://shirop.io/contexts/:context-id/doctypes/:doctype
+;; GET https://shirop.io/contexts/:context-id/current/:id
+;; GET https://shirop.io/contexts/:context-id/baseline/:id
+;; GET https://shirop.io/contexts/:context-id/stored/:id
+;; GET https://shirop.io/contexts/:context-id/history/:id
+;; GET https://shirop.io/contexts/:context-id/conflicted
+;; GET https://shirop.io/contexts/:context-id/documents/new
+;; GET https://shirop.io/contexts/:context-id/documents/new/:doctype
+;; GET https://shirop.io/contexts/:context-id/documents
+;; GET https://shirop.io/contexts/:context-id/documents/:doctype
+;; GET https://shirop.io/contexts/:context-id/selected/:selection-id
+;; GET https://shirop.io/contexts/:context-id/selected/:selection-id/:doctype
+
+;; all GET's have Expires: 0 or Cache-Control: no-cache in the header (except configurations, external-documents and doctype)
+
+;; Use HTTP response codes, e.g. 409 Conflict, 404 Not Found, 403 Permission Denied
+;; Use headers for return error messages, in Status Text or Warning header (actually not mandatory)
+
+;; Actually, using a REST representation and a Redis backend, we may not need
+;; the stateful stuff after all and the Redis atom thing; just assign Redis keys
+;; with :context-id as a prefix and use the purely functional layer
+;; (probably not directly, thin layer to only get the right portion of store
+;; from Redis and not the whole store at every step).
+
+;; The API moves away from the git semantics, but this could be easily recovered in the client libraries.
